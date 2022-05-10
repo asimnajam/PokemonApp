32fc4faed44c9da46a78b7a8881c5a1452e523ec
Asim <anajam@foree.co>
2022-05-10 22:33:31 +0500

    Initial Project Setup.

---
+++ PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Admin on 10/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
